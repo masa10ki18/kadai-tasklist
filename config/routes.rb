ebb5ae38f1c7499a41d4a9b73570e69377ad7a64
masaki.taniguchi2 <dmfrm212@gmail.com>
2018-08-18 12:58:01 +0000

    tasklist課題提出用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #トップとタスク一覧を同じページにする
+  root to: 'tasks#index'
+  
+  #ルーティング省略形
+  resources :tasks
+  
 end
