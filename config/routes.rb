0f76ee7e4514c9d683e9d6e08541de995f0fda42
masaki.taniguchi2 <dmfrm212@gmail.com>
2018-09-03 15:05:47 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'toppages/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   #トップとタスク一覧を同じページにする
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   #ルーティング省略形
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_134930) do
+ActiveRecord::Schema.define(version: 2018_09_03_142525) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
