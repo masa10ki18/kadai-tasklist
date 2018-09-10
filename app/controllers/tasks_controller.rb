b32f12e4885f91ab30b77865aa1db7e3cb609202
masaki.taniguchi2 <dmfrm212@gmail.com>
2018-09-10 14:38:45 +0000

    課題ログイン認証と一対多再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task , only: [:show, :edit, :update, :destroy] 
-  before_action :current_user , only: [:update, :destroy]
+  before_action :correct_user, only: [:update, :destroy]
   
   def new
     @task = Task.new
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      @task = current_user.tasks..order('created_at DESC').page(params[:page])
+      @task = current_user.tasks.order('created_at DESC').page(params[:page])
       flash[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -44,5 +44,12 @@ class TasksController < ApplicationController
   
     def task_params
       params.require(:task).permit(:content, :status ,:user_id)
+    end
+    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+      redirect_to root_url
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   #ルーティング:使用するアクションのみ
-  resources :tasks, only: [:new, :create, :update, :destroy]
+  resources :tasks, only: [:show, :new, :create, :edit, :update, :destroy]
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
